338d6ae6bbf7d5da7af1eb30cffd5216945c94a1
Yura <macho19972010@mail.ru>
2016-03-25 19:24:17 +0200

    Starting to create templates

---
+++ GitRemote/GitRemote/GitRemote/GeneralPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace GitRemote
+{
+    public partial class GeneralPage : NavigationPage
+    {
+        public GeneralPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
